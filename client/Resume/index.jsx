5b81b7dac614bd24909b8a4a0822c06d72c05872
joelbowen <jmichaelb722@gmail.com>
2016-08-30 00:06:53 -0400

    Accomplishments, Education, Experience
    also Improved some content margins on mobile and resume margins between scaling and fixed size transitions

--- client/Resume/index.jsx
+++ client/Resume/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Intro from '../Intro';
 import MyStack from '../MyStack';
-
+import Experience from '../Experience';
+import Accomplishments from '../Accomplishments';
+import Education from '../Education';
 import './resume.scss';
 
 /* Component */
@@ -10,6 +12,15 @@ function Resume() {
     <div id="resume" className="resume" itemType="http://schema.org/Person">
       <Intro />
       <MyStack />
+      <div className="row small-collapse">
+        <div className="small-10 medium-5 columns fill-stack">
+          <Experience />
+        </div>
+        <div className="small-10 medium-5 columns fill-stack">
+          <Accomplishments />
+          <Education />
+        </div>
+      </div>
     </div>
   );
 }
