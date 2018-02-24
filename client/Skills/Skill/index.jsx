044fd2d2380591586b0fdd60dad9b60d1b7cacb2
joelbowen <joel.michael.bowen@gmail.com>
2018-02-24 14:42:52 -0500

    feat: minor updates and improvements
    - Updated skills
    - Tweaked styles
    - Auto-calculate years of experience

--- client/Skills/Skill/index.jsx
+++ client/Skills/Skill/index.jsx
@@ -1,37 +1,41 @@
-import React from 'react';
+import React from 'react'
 
-import './skill.scss';
+import './skill.scss'
 
 /* Component */
 class Technology extends React.Component {
   render() {
-    const item = this.props.item;
-    const name = item ? item.name : '';
-    const subtitle = item ? item.subtitle : '';
+    const item = this.props.item
+    const name = item ? item.name : ''
+    const subtitle = item ? item.subtitle : ''
     const style = {
       backgroundColor: item ? item.hue : '#c3c3c3',
-      backgroundImage: `url(${item.image})`,
-      height: this.state && item ? `${item.proficiency}%` : '0%',
-    };
+      // backgroundImage: `url(${item.image})`,
+      height: this.state && item ? `${item.proficiency}%` : '0%'
+    }
     setTimeout(() => {
-      this.setState({ animate: true });
-    }, 25);
+      this.setState({ animate: true })
+    }, 25)
     return (
       <li className="skill">
         <hr />
         <hr />
         <hr />
-        <div className={`knowledge ${item.class ? item.class : null}`} style={style}>
-          <span className="title">{name}</span><br />
+        <div
+          className={`knowledge ${item.class ? item.class : null}`}
+          style={style}
+        >
+          <span className="title">{name}</span>
+          <br />
           <span className="subtitle">{subtitle}</span>
         </div>
       </li>
-    );
+    )
   }
 }
 
 Technology.propTypes = {
-  item: React.PropTypes.object,
-};
+  item: React.PropTypes.object
+}
 
-export default Technology;
+export default Technology
--- client/Skills/index.jsx
+++ client/Skills/index.jsx
@@ -1,43 +1,53 @@
-import React from 'react';
-import SkillGroup from './SkillGroup';
-import './skills.scss';
+import React from 'react'
+import moment from 'moment'
+import SkillGroup from './SkillGroup'
+import './skills.scss'
 
-const getSkill = (skills, type) => skills.filter((s) => s.type === type)
-                                         .sort((a, b) => a.proficiency < b.proficiency);
+const getSkill = (skills, type) =>
+  skills
+    .filter(s => s.type === type)
+    .sort((a, b) => a.proficiency < b.proficiency)
 /* Component */
 function Skills({ skills }) {
-  const frontEndSkills = getSkill(skills, 'front-end');
-  const serverSideSkills = getSkill(skills, 'server-side');
-  const databaseSkills = getSkill(skills, 'database');
-  const devOpsSkills = getSkill(skills, 'DevOps');
-  const otherSkills = getSkill(skills, 'Other');
-  const devOpsOtherSkills = devOpsSkills.concat(otherSkills).map((s) => s.name);
+  const frontEndSkills = getSkill(skills, 'front-end')
+  const serverSideSkills = getSkill(skills, 'server-side')
+  const databaseSkills = getSkill(skills, 'database')
+  const devOpsSkills = getSkill(skills, 'DevOps')
+  const otherSkills = getSkill(skills, 'Other')
+  const devOpsOtherSkills = devOpsSkills.concat(otherSkills).map(s => s.name)
+  const yearsExp = moment().diff('08-01-2004', 'years')
+  const frontEndDesc = `${yearsExp}-Years HTML/CSS, ${yearsExp -
+    3}-Years JavaScript`
+  const serverSideDesc = `${yearsExp -
+    8}-Years Server-side Javascript on Ubuntu / Linux with Node.js`
+  const databaseDesc = `${yearsExp -
+    8}-Years PostgreSQL, MongoDB, REDIS, and ${yearsExp - 4}-Years MySQL`
   return (
     <div className="skills">
       <SkillGroup
         technologies={frontEndSkills}
         title="Front-End"
-        description="12-Years HTML/CSS, 9-Years JavaScript, 4-Years AngularJS"
+        description={frontEndDesc}
       />
       <SkillGroup
         technologies={serverSideSkills}
         title="Server-Side"
-        description="4-Years Server-side Javascript on Ubuntu / Linux with Node.js & Express"
+        description={serverSideDesc}
       />
       <SkillGroup
         technologies={databaseSkills}
         title="Databases"
-        description="4-Years PostgreSQL, MongoDB, REDIS, and 8-Years MySQL"
+        description={databaseDesc}
       />
       <p className="other-skills">
-        OTHER SKILLS: { devOpsOtherSkills.join(', ') }
+        OTHER SKILLS: {devOpsOtherSkills.join(', ')}
       </p>
     </div>
-  );
+  )
 }
 
 Skills.propTypes = {
-  skills: React.PropTypes.array.isRequired,
-};
+  skills: React.PropTypes.array.isRequired
+}
 
-export default Skills;
+export default Skills
