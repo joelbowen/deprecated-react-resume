a235eeb0775d46566d3d77a281de18e871c6c907
joelbowen <joel.michael.bowen@gmail.com>
2018-02-24 17:58:13 -0500

    feat: service worker / offline support

--- gulp/tasks/webpack-build.js
+++ gulp/tasks/webpack-build.js
@@ -1,27 +1,30 @@
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const webpack = require('webpack');
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const webpack = require('webpack')
 
-const webpackConfig = Object.create(require('../../webpack.config'));
+const webpackConfig = Object.create(require('../../webpack.config'))
 
-gulp.task('webpack:build', (callback) => {
+gulp.task('webpack:build', callback => {
   webpackConfig.plugins = webpackConfig.plugins.concat(
     new webpack.DefinePlugin({
       'process.env': {
         BROWSER: true,
-        NODE_ENV: JSON.stringify('production'),
-      },
+        NODE_ENV: JSON.stringify('production')
+      }
     }),
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.UglifyJsPlugin()
-	);
+    new webpack.optimize.DedupePlugin()
+    // new webpack.optimize.UglifyJsPlugin()
+  )
 
-	// run webpack
+  // run webpack
   webpack(webpackConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError('webpack:build', err);
-    gutil.log('[webpack:build]', stats.toString({
-      colors: true,
-    }));
-    callback();
-  });
-});
+    if (err) throw new gutil.PluginError('webpack:build', err)
+    gutil.log(
+      '[webpack:build]',
+      stats.toString({
+        colors: true
+      })
+    )
+    callback()
+  })
+})
